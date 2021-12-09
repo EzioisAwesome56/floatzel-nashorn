197a235a69f13af2533d231322ad75653f87783b
EzioisAwesome56 <jedijon567@gmail.com>
2021-12-08 20:30:42 -0500

    update loading mechcanism

--- src/main/java/com/eziosoft/floatzel/nashorn/batch.java
+++
@@ -1,9 +0,0 @@
-package com.eziosoft.floatzel.nashorn;
-
-public class batch
-{
-    public static String[] getClasses(){
-        return new String[]{"com.eziosoft.floatzel.nashorn.cmd.eval",
-                "com.eziosoft.floatzel.nashorn.cmd.plugapi"};
-    }
-}
